416022176b138a7c3ef2e29f9ed6ba8632d6ac08
Ronan Leroy <ronan.leroy@gmail.com>
2018-08-08 14:08:11 +0200

    Default filename to original filename if no title is present

--- src/importer/markdown.js
+++ src/importer/markdown.js
@@ -15,7 +15,7 @@ class MarkdownImporter {
     return new Promise((resolve, reject) => {
       fs.readFile(`${sourceFolder}/${filename}`, 'utf8', (err, markdown) => {
         fs.rename(`${sourceFolder}/${filename}`, `${settings.archiveFolder}/${filename}`, (err) => {/* Maybe do something here */});
-        const article = this.processMarkdown(markdown);
+        const article = this.processMarkdown(markdown, filename);
         const pubDate = new Date(article.metadata.pubDate);
         const newFilename = `${pubDate.getFullYear()}-${`0${pubDate.getMonth()+1}`.slice(-2)}-${`0${pubDate.getDate()}`.slice(-2)}-${`0${pubDate.getHours()}`.slice(-2)}-${`0${pubDate.getMinutes()}`.slice(-2)}-${filename}`;
         fs.writeFile(`${settings.dataFolder}/${newFilename}`, article.data, (err) => {
@@ -29,7 +29,7 @@ class MarkdownImporter {
     });
   }
 
-  processMarkdown(markDown) {
+  processMarkdown(markDown, filename) {
     let data;
 
     // We don’t use the html, but we must process the markdown in order to access the metadata.
@@ -38,12 +38,12 @@ class MarkdownImporter {
     let metadata = yaml.load(rawMetadata);
 
     if (metadata !== null) {
-      this.fillMetadata(metadata, markDown);
+      this.fillMetadata(metadata, markDown, filename);
 
       data = markDown.replace(rawMetadata, yaml.dump(metadata));
     } else {
       metadata = {};
-      this.fillMetadata(metadata, markDown);
+      this.fillMetadata(metadata, markDown, filename);
 
       data = `---\n${yaml.dump(metadata)}---\n${markDown}`;
     }
@@ -51,7 +51,7 @@ class MarkdownImporter {
     return {metadata, data};
   }
 
-  fillMetadata(metadata, markDown) {
+  fillMetadata(metadata, markDown, filename) {
     if (typeof metadata.pubDate === 'undefined' || !metadata.pubDate) {
       const date = new Date();
       metadata.pubDate = date.toGMTString();
@@ -74,6 +74,8 @@ class MarkdownImporter {
       metadata.filename = metadata.title.trim().toLowerCase()
         .replace(/[!@#$%^&*(),.?":{}|<>_\\\/\- ]+/g, '-')
         .replace(/(^-|-$)/g, '');
+    } else {
+      metadata.filename = filename.replace(/\.[a-zA-Z0-9]+$/, '');
     }
   }
 }
